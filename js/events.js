73ecf79cb1c484a50b59be561b81f243f9ed9678
Cameron Malagar <cmalagar5@gmail.com>
2016-09-08 13:43:06 -0400

    events page

--- js/events.js
+++ js/events.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
 	var addEventButton = $(".js-add-event-button");
+	var editEventButton = $(".js-edit-event-button");
 	var addEventPanel = $(".js-add-event-panel");	
+	var editEventPanel = $(".js-edit-event-panel");
 	var cancelAddEventButton = $(".js-cancel-add-event-button");
+	var cancelEditEventButton = $(".js-cancel-edit-event-button");
 
 	addEventButton.click(function() {
 		addEventPanel.removeClass("hidden");
 	});
 
+	editEventButton.click(function() {
+		editEventPanel.removeClass("hidden");
+	});
+
 	cancelAddEventButton.click(function() {
 		addEventPanel.addClass("hidden");
+	});
+
+	cancelEditEventButton.click(function() {
+		editEventPanel.addClass("hidden");
 	});
 
 });
