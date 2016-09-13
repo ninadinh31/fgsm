fd01eb6dc8282083ba6b9e6b2c700bf694f6a280
Cameron Malagar <cmalagar5@gmail.com>
2016-09-13 13:22:54 -0400

    add events

--- addevents.php
+++ addevents.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>
-		Federal & Global Fellows Events
+		Federal & Global Fellows Add Events
 	</title>
 	<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/style.css" rel="stylesheet" type="text/css">
@@ -44,19 +44,20 @@
 						</div>
 					</div>
 					<div class="form-group">
-					<label for="event_name">Event Name:</label>
-					<input type="text" class="form-control" id="event_name">
+						<label for="event_name">Event Name:</label>
+						<input type="text" class="form-control" id="event_name">
+					</div>
+					<div class="form-group">
+						<label for="event_description">Description:</label>
+						<input type="text" class="form-control" id="event_description">
+					</div>	
+					<div class="form-group">
+						<label for="event_rsvp_link">RSVP Link:</label>
+						<input type="text" class="form-control" id="event_rsvp_link">
+					</div>
+					<button type="button" class="btn btn-primary js-final-add-event-button">Add Event</button>
+					<button type="button" class="btn btn-danger js-cancel-add-event-button">Cancel</button>
 				</div>
-				<div class="form-group">
-					<label for="event_description">Description:</label>
-					<input type="text" class="form-control" id="event_description">
-				</div>	
-				<div class="form-group">
-					<label for="event_rsvp_link">RSVP Link:</label>
-					<input type="text" class="form-control" id="event_rsvp_link">
-				</div>
-				<button type="button" class="btn btn-primary js-final-add-event-button">Add Event</button>
-				<button type="button" class="btn btn-danger js-cancel-add-event-button">Cancel</button>
 			</div>
 		</div>
 	</div>
