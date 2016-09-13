0ee7b5bdf75b1018e7c5651e604cbc6060d67e62
Cameron Malagar <cmalagar5@gmail.com>
2016-09-13 13:01:01 -0400

    add events separate

---
+++ addevents.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Federal & Global Fellows Events
+	</title>
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <script src="js/jquery-1.11.3.min.js"></script>
+    <script src="js/events.js"></script>
+</head>	
+<body>
+	<div class="panel panel-default js-add-event-panel hidden">
+		<div class="panel-heading">Add Event</div>
+		<div class="panel-body">
+			<div class="row">
+				<div class="col-md-3">
+					<div class="form-group">
+						<label for="date">Date:</label>
+						<input type="date" class="form-control" id="date">
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="form-group">
+						<label for="start_time">Start Time:</label>
+						<input type="time" class="form-control" id="start_time">
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="form-group">
+						<label for="end_time">End Time:</label>
+						<input type="time" class="form-control" id="end_time">
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="form-group">
+						<label for="location">Location:</label>
+						<input type="text" class="form-control" id="location">
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="event_name">Event Name:</label>
+				<input type="text" class="form-control" id="event_name">
+			</div>
+			<div class="form-group">
+				<label for="event_description">Description:</label>
+				<input type="text" class="form-control" id="event_description">
+			</div>	
+			<div class="form-group">
+				<label for="event_rsvp_link">RSVP Link:</label>
+				<input type="text" class="form-control" id="event_rsvp_link">
+			</div>
+			<button type="button" class="btn btn-primary js-final-add-event-button">Add Event</button>
+			<button type="button" class="btn btn-danger js-cancel-add-event-button">Cancel</button>
+		</div>
+	</div>
+</body>		
