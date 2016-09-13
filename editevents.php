adbf2c674d50d9aa8b5f673fa5e1852fb09baaae
Cameron Malagar <cmalagar5@gmail.com>
2016-09-13 13:31:37 -0400

    add events

---
+++ editevents.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Federal & Global Fellows Edit Events
+	</title>
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <script src="js/jquery-1.11.3.min.js"></script>
+    <script src="js/events.js"></script>
+</head>	
+<body>
+	<div class="row">
+		<div class="col-md-2"></div>
+		<div class="col-md-8">
+			<div class="panel panel-default js-edit-event-panel">
+				<div class="panel-heading">Edit Event</div>
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-md-3">
+							<div class="form-group">
+								<label for="date">Date:</label>
+								<input type="date" class="form-control" id="date">
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="form-group">
+								<label for="start_time">Start Time:</label>
+								<input type="time" class="form-control" id="start_time">
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="form-group">
+								<label for="end_time">End Time:</label>
+								<input type="time" class="form-control" id="end_time">
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="form-group">
+								<label for="location">Location:</label>
+								<input type="text" class="form-control" id="location">
+							</div>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="event_name">Event Name:</label>
+						<input type="text" class="form-control" id="event_name">
+					</div>
+					<div class="form-group">
+						<label for="event_description">Description:</label>
+						<input type="text" class="form-control" id="event_description">
+					</div>	
+					<div class="form-group">
+						<label for="event_rsvp_link">RSVP Link:</label>
+						<input type="text" class="form-control" id="event_rsvp_link">
+					</div>
+					<button type="button" class="btn btn-primary js-final-edit-event-button">Add Event</button>
+					<button type="button" class="btn btn-danger js-cancel-edit-event-button">Cancel</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>		
+</html>
