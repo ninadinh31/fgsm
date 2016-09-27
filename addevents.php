63531451285cd31c9245a0b17f56d4ace3d7dd16
Cameron Malagar <cmalagar5@gmail.com>
2016-09-27 12:03:50 -0400

    events page

--- addevents.php
+++ addevents.php
@@ -9,65 +9,57 @@
 require_once('Connections/FGSP.php');
 require_once('includes/header.php');
 
-function()
-
-mysql_select_db($database_FGSP, $FGSP);
-$sql = 'INSERT INTO tblEvents
-			(EventDate, EventName, EventYear, EventLocation, EventType, EventAttendanceLink,
-			 EventStartTime, EventEndTime, EventDescription)
-		VALUES 
-			();';
-
-// $result = mysql_query($sql, $FGSP) or die(mysql_error());
 ?>
 
 	<div class="row">
 		<div class="panel panel-default">
 			<div class="panel-heading">Add Event</div>
 			<div class="panel-body">
-				<div class="row">
-					<div class="col-md-3">
-						<div class="form-group">
-							<label for="date">Date:</label>
-							<input type="date" class="form-control" id="date">
+				<form action="insertevent.php" method="post">
+					<div class="row">
+						<div class="col-md-3">
+							<div class="form-group">
+								<label for="date">Date:</label>
+								<input type="date" class="form-control" id="date">
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="form-group">
+								<label for="start_time">Start Time:</label>
+								<input type="time" class="form-control" id="start_time">
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="form-group">
+								<label for="end_time">End Time:</label>
+								<input type="time" class="form-control" id="end_time">
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="form-group">
+								<label for="location">Location:</label>
+								<input type="text" class="form-control" id="location">
+							</div>
 						</div>
 					</div>
-					<div class="col-md-3">
-						<div class="form-group">
-							<label for="start_time">Start Time:</label>
-							<input type="time" class="form-control" id="start_time">
-						</div>
+					<div class="form-group">
+						<label for="event_name">Event Name:</label>
+						<input type="text" class="form-control" id="event_name">
 					</div>
-					<div class="col-md-3">
-						<div class="form-group">
-							<label for="end_time">End Time:</label>
-							<input type="time" class="form-control" id="end_time">
-						</div>
+					<div class="form-group">
+						<label for="event_type">Event Type:</label>
+						<input type="text" class="form-control" id="event_type">
 					</div>
-					<div class="col-md-3">
-						<div class="form-group">
-							<label for="location">Location:</label>
-							<input type="text" class="form-control" id="location">
-						</div>
+					<div class="form-group">
+						<label for="event_description">Description:</label>
+						<input type="text" class="form-control" id="event_description">
+					</div>	
+					<div class="form-group">
+						<label for="event_rsvp_link">RSVP Link:</label>
+						<input type="text" class="form-control" id="event_rsvp_link">
 					</div>
-				</div>
-				<div class="form-group">
-					<label for="event_name">Event Name:</label>
-					<input type="text" class="form-control" id="event_name">
-				</div>
-				<div class="form-group">
-					<label for="event_type">Event Type:</label>
-					<input type="text" class="form-control" id="event_type">
-				</div>
-				<div class="form-group">
-					<label for="event_description">Description:</label>
-					<input type="text" class="form-control" id="event_description">
-				</div>	
-				<div class="form-group">
-					<label for="event_rsvp_link">RSVP Link:</label>
-					<input type="text" class="form-control" id="event_rsvp_link">
-				</div>
-				<button type="submit" class="btn btn-primary js-add-event-button">Add Event</button>
+					<button type="submit" class="btn btn-primary js-add-event-button">Add Event</button>
+				</form>
 			</div>
 		</div>
 	</div>
