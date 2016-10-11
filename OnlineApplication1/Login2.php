43ccf32a5c2eb893609a99f246d78abe54fa3612
Cameron Malagar <cmalagar5@gmail.com>
2016-10-11 12:28:16 -0400

    login page

--- OnlineApplication1/Login2.php
+++ OnlineApplication1/Login2.php
@@ -51,7 +51,8 @@ require_once('../includes/header.php');
 <div class="row">
     <div class="col-md-4"></div>
     <div class="col-md-4">
-        <div class="panel">
+        <div class="panel panel-default">
+            <div class="panel-heading">Log</div>
             <form ACTION="<?php echo $loginFormAction; ?>" id="frmLogin" name="frmLogin" method="POST">
                 <table>
                     <tbody>
