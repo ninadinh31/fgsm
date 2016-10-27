3f0c199489a3fbe87b0b1c120ff7448cd229f5ab
Cameron Malagar <cmalagar5@gmail.com>
2016-10-27 13:10:04 -0400

    control panel

--- controlpanel.php
+++ controlpanel.php
@@ -8,13 +8,13 @@ require_once('includes/header.php');
 <div class="row">
 	<div class="col-md-12">
 		<div class="panel panel-default">
-			<div class="panel-heading"></div>
+			<div class="panel-heading">Control Panel</div>
 			<div class="panel-body">
-				<a href="">Federal & Global Fellows Events</a>
-				<a href="">Admin Add Event</a>
-				<a href="">Admin Edit/Delete Event</a>
-				<a href="">Internship Opportunities</a>
-				<a href="">Federal & Global Fellows Application</a>
+				<a href="">Federal & Global Fellows Events</a><br>
+				<a href="">Admin Add Event</a><br>
+				<a href="">Admin Edit/Delete Event</a><br>
+				<a href="">Internship Opportunities</a><br>
+				<a href="">Federal & Global Fellows Application</a><br>
 			</div>
 		</div>
 	</div>
