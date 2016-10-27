1e1a347d5ea2c6d533987cf8859aa64662f7b8d3
Cameron Malagar <cmalagar5@gmail.com>
2016-10-27 13:37:49 -0400

    control panel

--- controlpanel.php
+++ controlpanel.php
@@ -3,6 +3,60 @@
 require_once('Connections/NewLogin.php'); 
 require_once('includes/header.php');
 
+// *** Validate request to login to this site.
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+/**
+ * Determines if the current user is authorized to access the login page
+ */
+function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) { 
+	// For security, start by assuming the visitor is NOT authorized. 
+	$isValid = False; 
+
+	// When a visitor has logged into this site, the Session variable MM_Username set equal to their username. 
+	// Therefore, we know that a user is NOT logged in if that Session variable is blank. 
+	if (!empty($UserName)) { 
+  		// Besides being logged in, you may restrict access to only certain users based on an ID established when they login.   
+  		// Parse the strings into arrays. 
+  		$arrUsers = Explode(",", $strUsers); 
+  		$arrGroups = Explode(",", $strGroups); 
+  		if (in_array($UserName, $arrUsers)) { 
+    		$isValid = true; 
+  		} 
+  		
+  		// Or, you may restrict access to only certain users based on their username. 
+  		if (in_array($UserGroup, $arrGroups)) { 
+    		$isValid = true; 
+  		} 
+  		
+  		if (($strUsers == "") && false) { 
+    		$isValid = true; 
+  		} 
+	} 
+	
+	return $isValid; 
+}
+
+$MM_restrictGoTo = "login.php";
+if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {   
+	$MM_qsChar = "?";
+	$MM_referrer = $_SERVER['PHP_SELF'];
+	
+	if (strpos($MM_restrictGoTo, "?")) 
+		$MM_qsChar = "&";
+    
+    if (isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) > 0) 
+    	$MM_referrer .= "?" . $_SERVER['QUERY_STRING'];
+    
+    $MM_restrictGoTo = $MM_restrictGoTo. $MM_qsChar . "accesscheck=" . urlencode($MM_referrer);
+    echo "You don't have access to this page.";
+    header("Location: ". $MM_restrictGoTo); 
+    exit;
+}
+
+
 ?>
 
 <div class="row">
@@ -10,9 +64,9 @@ require_once('includes/header.php');
 		<div class="panel panel-default">
 			<div class="panel-heading">Control Panel</div>
 			<div class="panel-body">
-				<a href="">Federal & Global Fellows Events</a><br>
-				<a href="">Admin Add Event</a><br>
-				<a href="">Admin Edit/Delete Event</a><br>
+				<a href="events.php">Federal & Global Fellows Events</a><br>
+				<a href="addevents.php">Admin Add Event</a><br>
+				<a href="editevents.php">Admin Edit/Delete Event</a><br>
 				<a href="">Internship Opportunities</a><br>
 				<a href="">Federal & Global Fellows Application</a><br>
 			</div>
