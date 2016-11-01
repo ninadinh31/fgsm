2faf9cc399466ab3252320e852e27c2780450ebf
Cameron Malagar <cmalagar5@gmail.com>
2016-11-01 12:42:56 -0400

    control panel

--- js/controlpanel.js
+++ js/controlpanel.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
-	var username = $('js-username');
-	var usergroup = $('js-usergroup');
-	var addEvents = $('js-add-events');
-	var editEvents = $('js-edit-events');
+	var username = $('.js-username');
+	var usergroup = $('.js-usergroup');
+	var addEvents = $('.js-add-events');
+	var editEvents = $('.js-edit-events');
 
 
 	alert(usergroup.html());
