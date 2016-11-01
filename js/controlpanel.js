0718c1e043cf07755493933c93bc23fb795ad5ce
Cameron Malagar <cmalagar5@gmail.com>
2016-11-01 12:20:38 -0400

    control panel

--- js/controlpanel.js
+++ js/controlpanel.js
@@ -6,11 +6,13 @@ $(document).ready(function() {
 	var editEvents = $('js-edit-events');
 
 
-	alert(usergroup);
-	
+	alert(usergroup.text());
+
 	if (usergroup.text() === '1') {
 		addEvents.show();
 		editEvents.show();
+	} else {
+		alert(hello);
 	}
 
 });
