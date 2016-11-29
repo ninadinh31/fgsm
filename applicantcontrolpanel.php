1204007f266702a80ab75409c7d184447402c0b1
Cameron Malagar <cmalagar5@gmail.com>
2016-11-29 12:43:04 -0500

    control panel

--- applicantcontrolpanel.php
+++ applicantcontrolpanel.php
@@ -67,6 +67,9 @@ require_once('includes/header.php');
 			<div class="panel-heading">Applicant Control Panel</div>
 			<div class="panel-body">
 				<div>
+					<p>Welcome <?php echo $_SESSION['MM_Username']; ?></p>
+				</div>
+				<div>
 					<a href="">Federal & Global Fellows Application</a>
 				</div>
 			</div>
