e875498fe0b6d04727e730fec6993106bd24a2a7
Cameron Malagar <cmalagar5@gmail.com>
2016-12-15 12:49:16 -0500

    modularized tabs of the application

--- includes/header.php
+++ includes/header.php
@@ -352,6 +352,6 @@ font-size: 17px;
     
  	<div class="row" align="center" style="margin:0px;background-color:#000;">
     	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="align-content:center;background-color:#000; ">
-  			<a href="http://www.globalsemesterdc.umd.edu"><img src="images/globalsemester1000w.jpg"  class="img-responsive" alt="Global Fellows in Washington, DC icon and banner" height="333" hspace="0" vspace="0" border="0"></a>
+  			<a href="http://www.globalsemesterdc.umd.edu"><img src="../../images/MirishShah.png"  class="img-responsive" alt="Global Fellows in Washington, DC icon and banner" height="333" hspace="0" vspace="0" border="0"></a>
     	</div>
     </div>
